2839dae26c1efb0442757f1d91d61939ba53210f
thomasbrettell <thomas.a.brettell@gmail.com>
2021-12-05 21:48:59 +1100

    add regression test for long text task

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -32,3 +32,11 @@ Archived.args = {
     state: 'TASK_ARCHIVED',
   },
 };
+
+export const LongText = Template.bind({});
+LongText.args = {
+  task: {
+    ...Default.args.task,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum rhoncus nunc, id scelerisque neque posuere ac. Quisque sapien nunc, cursus a auctor a, volutpat sit amet neque. Praesent sit amet dolor lacus. Ut nec vulputate ligula, at ornare enim. Duis ac lectus et sapien blandit scelerisque. Etiam dapibus mauris vel eros auctor, ac varius nisi elementum. Aliquam sit amet efficitur purus, nec pharetra dolor. Nulla tempor neque sit amet ex pulvinar tincidunt. Pellentesque malesuada felis risus, ac hendrerit ante scelerisque ut. Maecenas vitae massa mauris. Morbi venenatis lorem et tristique pulvinar. Curabitur auctor cursus massa, auctor fermentum enim venenatis vel. Sed sed magna risus. Sed aliquet egestas felis, non convallis mi laoreet at. Phasellus quis sodales turpis, et scelerisque lectus. Donec vitae elit quis libero venenatis convallis.',
+  },
+};
