636c5779e8cf37576eb16bcc6fa3cb8a5449dd4f
thomasbrettell <thomas.a.brettell@gmail.com>
2021-12-05 17:56:49 +1100

    tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDir: ["../public"],
   addons: [
     "@storybook/addon-links",
